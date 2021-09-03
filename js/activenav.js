bedc8c7f43ce5df0b1d265397f909bd6f4ada92a
tyii <shamanshows@gmail.com>
2021-09-03 16:38:06 -0400

    Update activenav.js

--- js/activenav.js
+++ js/activenav.js
@@ -1,5 +1,7 @@
-$("a.navlink").click(function(){
-  $("a.navlink").css("background-color", "#5C507F");
-  $(this).css("background-color", "#3A3961");
-  $(this).css("color", "#FFEC77");
-  });
+<script type="text/javascript">
+  $("a.navlink").click(function(){
+    $("a.navlink").css("background-color", "#5C507F");
+    $(this).css("background-color", "#3A3961");
+    $(this).css("color", "#FFEC77");
+    });
+</script>
