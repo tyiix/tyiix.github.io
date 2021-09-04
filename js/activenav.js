d1d08fd9968f5c63c34c247c4a475c5077e92be5
shows <shamanshows@gmail.com>
2021-09-03 20:41:16 -0400

    a

--- js/activenav.js
+++ js/activenav.js
@@ -1,9 +1,1 @@
-navbar = document.querySelector(".navbar").querySelectorAll("a");
-
-navbar.forEach(element => {
-	element.addEventlistener("click",function(){
-		navbar.forEach(nav=>nav.classList.remove("active"))
-
-		this.classList.add("active");
-	})
-});
+//* TBA until i figure this out
