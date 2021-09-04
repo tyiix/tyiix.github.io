793940e1e2f4145cbf3b45ea1ad015827cdc9e24
shows <shamanshows@gmail.com>
2021-09-04 16:41:45 -0400

    minor change

--- js/navreplace2.js
+++ js/navreplace2.js
@@ -1,5 +1,5 @@
 /* Only for folder within folder */
 $(function(){
-    $("#nav-placeholder").load("../../template/nav.html");
+    $("#nav-placeholder").load("../../template/nav2.html");
   });
   
